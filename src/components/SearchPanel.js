c0b29c5ff92a043ea7da370bb7385fde9fa0b2f1
JIetForce <45129533+JIetForce@users.noreply.github.com>
2019-10-08 14:25:49 +0300

    Aded styles

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const SearchPanel = () => {
-  const searchText = 'Tap here to search';
-  const searchStyle = {
-    fontSize: '20px'
-  };
+import './SearchPanel.css';
 
-  return <input 
-    style = {searchStyle} 
-    placeholder = {searchText} />;
+const SearchPanel = () => {
+  return (
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" />
+  );
 };
 
 export default SearchPanel;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import TodoListItem from './TodoListItem';
 
-const TodoList = () => {
-  const items = ['Drink coffee', 'Build Awesome App'];
+import TodoListItem from './TodoListItem';
+import './TodoList.css';
+
+const TodoList = ({ todos }) => {
+
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
+    );
+  });
 
   return (
-    <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
+    <ul className="list-group todo-list">
+      { elements }
     </ul>
   );
 };
