ba57517f8e53728bc69e289c39a2015dd9e964d4
JIetForce <45129533+JIetForce@users.noreply.github.com>
2019-10-13 14:37:26 +0300

    Done

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,34 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import './TodoListItem.css';
 
-export default class TodoListItem extends Component {
+const TodoListItem = ({ label, onDeleted,
+                        onToggleImportant,
+                        onToggleDone,
+                        important, done }) => {
 
-  state = {
-    done: false,
-    important: false
-  };
-
-  onLabelClick = () => {
-    this.setState(({done}) => {
-      return {
-        done: !done
-      };
-    });
-  };
-
-  onMarkImportant = () => {
-    this.setState(({important}) => {
-      return {
-        important: !important
-      };
-    });
-  };
-
-  render() {
-    const { label } = this.props;
-    const { done, important } = this.state;
-
+    
     let classNames = "todo-list-item";
     if(done) {classNames += ' done'};
 
@@ -38,22 +17,23 @@ export default class TodoListItem extends Component {
       <span className={classNames} >
         <span
           className="todo-list-item-label"
-          onClick={this.onLabelClick}>
+          onClick={onToggleDone}>
           {label}
         </span>
   
-        <button type="button"
+        <button type="button" 
                 className="btn btn-outline-success btn-sm float-right"
-                onClick={this.onMarkImportant}>
+                onClick={onToggleImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
     );
-  };
 };
 
+export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import AppHeader from './components/AppHeader/AppHeader';
-import SearchPanel from './components/SearchPanel/SearchPanel';
-import TodoList from './components/TodoList/TodoList';
-import ItemStatusFilter from './components/ItemStatusFilter/ItemStatusFilter';
-
+import App from './components/App/App';
 import './Index.css';
 
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
 
 ReactDOM.render(<App />,
   document.getElementById('root'));
